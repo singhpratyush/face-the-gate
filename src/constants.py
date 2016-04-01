8242a18df66a7057f5e51144969cde9c8fa6519d
Pratyush Singh <singh.pratyush96@gmail.com>
2016-04-01 17:16:47 +0530

    Added some constants

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+EIGEN_RECOGNIZER_PATH = 'rsc/recognition_files/eigen_recognizer.yml'
+FISHER_RECOGNIZER_PATH = 'rsc/recognition_files/fisher_recognizer.yml'
+LBHP_RECOGNIZER_PATH = 'rsc/recognition_files/lbhp_recognizer.yml'
